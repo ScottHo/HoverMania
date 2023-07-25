4bf6f21d7b2e4b81cf16d48e183b053d11ce55e2
Scott Ho <sscott.ho@gmail.com>
2023-07-25 16:08:29 -0600

    Greatly Improve Car Physics, add a jump booster to the car

--- Assets/Scripts/CarControllerScript.cs
+++ Assets/Scripts/CarControllerScript.cs
@@ -1,12 +1,17 @@
+using Codice.CM.Common;
+using System;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
     public AxleInfo[] axleInfos;
     public Rigidbody rigidBody;
-    float torque = 360;
-    float angle = 35;
-    float brakeTorque = 1000;
+    float torque = 1000;
+    float angle = 45;
+    float brakeTorque = 2000;
+    public float jumpPower = 100000;
+    public float driftPower = 1000;
+    public float maxSpeed = 250;
     LogicScript logic;
 
     void Start()
@@ -17,51 +22,120 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
-        float motor = torque * Input.GetAxis("Vertical");
+        Drive();
+        ApplyBooster();
+    }
 
-        float steering = angle * Input.GetAxis("Horizontal");
-
+    void ApplyBooster()
+    {
+        bool grounded = false;
         foreach (AxleInfo axleInfo in axleInfos)
         {
-            if (axleInfo.steering)
+            grounded |= axleInfo.leftWheel.isGrounded;
+            grounded |= axleInfo.rightWheel.isGrounded;
+        }
+        if (grounded)
+        {
+            //rigidBody.constraints = RigidbodyConstraints.None;
+            if (Input.GetKey(KeyCode.Space))
             {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
+                rigidBody.AddForce(Vector3.up * jumpPower);
             }
-            if (axleInfo.motor)
-            {
-                bool isMovingForward = axleInfo.leftWheel.rotationSpeed > 5;
-                bool reverse = motor < 0;
-                if (motor == 0 || (reverse && isMovingForward))
-                {
-                    axleInfo.leftWheel.brakeTorque = brakeTorque;
-                    axleInfo.rightWheel.brakeTorque = brakeTorque;
-                    axleInfo.leftWheel.motorTorque = 0;
-                    axleInfo.rightWheel.motorTorque = 0;
-                    logic.SetBatteryDraining(false);
+        }
+        else
+        {
+            //rigidBody.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+            float drift = Input.GetAxis("Horizontal");
+            Vector3 angularVelocity = rigidBody.angularVelocity;
+            angularVelocity.y = drift/3;
+            rigidBody.angularVelocity = angularVelocity;
+        }
+        LimitAngularVelocity();
+    }
 
-                }
-                else
-                {
-                    logic.SetBatteryDraining(true);
-                    axleInfo.leftWheel.brakeTorque = 0;
-                    axleInfo.rightWheel.brakeTorque = 0;
-                    if (reverse)
-                    {
-                        axleInfo.leftWheel.motorTorque = motor * (float) .75;
-                        axleInfo.rightWheel.motorTorque = motor * (float) .75;
-                    }
-                    else
-                    {
-                        axleInfo.leftWheel.motorTorque = motor;
-                        axleInfo.rightWheel.motorTorque = motor;
-                    }
-                }
-            }
+    void LimitAngularVelocity()
+    {
+        Vector3 angularVelocity = rigidBody.angularVelocity;
+        angularVelocity.x = Mathf.Clamp(angularVelocity.x, -.1f, .1f);
+        angularVelocity.z = Mathf.Clamp(angularVelocity.z, -.1f, .1f);
+        angularVelocity.y = Mathf.Clamp(angularVelocity.y, -angle*(float)Math.PI / 180.0f, angle*(float)Math.PI/180.0f);
+        rigidBody.angularVelocity = angularVelocity;
+    }
+
+    void Drive()
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            Steer(axleInfo);
+            Motor(axleInfo);
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
     }
+
+    void Steer(AxleInfo axleInfo)
+    {
+        float steering = angle * Input.GetAxis("Horizontal");
+        if (axleInfo.steering)
+        {
+            axleInfo.leftWheel.steerAngle = steering;
+            axleInfo.rightWheel.steerAngle = steering;
+        }
+    }
+
+    void Motor(AxleInfo axleInfo)
+    {
+        float motor = torque * Input.GetAxis("Vertical");
+        if (axleInfo.motor)
+        {
+            bool isMovingForward = axleInfo.leftWheel.rotationSpeed > 5;
+            bool isMovingBackwards = axleInfo.leftWheel.rotationSpeed < -5;
+            bool reverse = motor < 0;
+            if (motor == 0 || (reverse && isMovingForward) || (!reverse && isMovingBackwards))
+            {
+                logic.SetBatteryDraining(false);
+                axleInfo.leftWheel.brakeTorque = brakeTorque;
+                axleInfo.rightWheel.brakeTorque = brakeTorque;
+                axleInfo.leftWheel.motorTorque = 0;
+                axleInfo.rightWheel.motorTorque = 0;
+            }
+            else
+            {
+                logic.SetBatteryDraining(true);
+                axleInfo.leftWheel.brakeTorque = 0;
+                axleInfo.rightWheel.brakeTorque = 0;
+
+                float speed = Mathf.Abs(rigidBody.velocity.sqrMagnitude);
+                if (reverse)
+                {
+                    if (speed < maxSpeed * .5)
+                    {
+                        axleInfo.leftWheel.motorTorque = motor;
+                        axleInfo.rightWheel.motorTorque = motor;
+                    }
+                    else 
+                    {
+                        axleInfo.leftWheel.motorTorque = 0;
+                        axleInfo.rightWheel.motorTorque = 0;
+                    }
+                }
+                else
+                {
+                    if (speed < maxSpeed)
+                    {
+                        axleInfo.leftWheel.motorTorque = motor;
+                        axleInfo.rightWheel.motorTorque = motor;
+                    }
+                    else
+                    {
+                        axleInfo.leftWheel.motorTorque = 0;
+                        axleInfo.rightWheel.motorTorque = 0;
+                    }
+                }
+            }
+        }
+    }
+
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
         if (collider.transform.childCount == 0)
