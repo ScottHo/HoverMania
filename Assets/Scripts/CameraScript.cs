752d39b6a266cb7368cb7dfee3eb5730746d29e9
Scott Ho <sscott.ho@gmail.com>
2023-08-01 04:06:04 -0600

    Modify the camera to view obstructions as transparent

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform obstrution;
+    public Transform target;
+    Color lastColor;
+
+    private void LateUpdate()
+    {
+        ViewObstructed();
+    }
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 10f))
+        {
+            int terrainLayer = 6;
+            if (hit.collider.gameObject.layer == terrainLayer)
+            {
+                if (obstrution != null && obstrution != hit.transform)
+                {
+                    // Obstruction is a continuous hit on terrain layer, revert old obstruction first
+                    SetObstructionOpaque();
+                    obstrution = hit.collider.transform;
+                    SetObstructionTransparent();
+                    return;
+                }
+                else if (obstrution == null)
+                {
+                    // Obstruction is a new hit on terrain layer
+                    obstrution = hit.collider.transform;
+                    SetObstructionTransparent();
+                    return;
+                }
+                else if (obstrution == hit.transform)
+                {
+                    // Obstruction is same object, continue
+                    return;
+                } 
+            }
+        }
+        SetObstructionOpaque();
+    }
+    void SetObstructionOpaque()
+    {
+        if (obstrution != null)
+        {
+            obstrution.gameObject.GetComponent<MeshRenderer>().material.color = lastColor;
+            Utils.SetupMaterialWithBlendMode(obstrution.gameObject.GetComponent<MeshRenderer>().material, Utils.BlendMode.Opaque, true);
+            obstrution = null;
+        }
+    }
+    void SetObstructionTransparent()
+    {
+        if (obstrution != null)
+        {
+            // Store the previous color before modifying it's transparency
+            Color color = obstrution.gameObject.GetComponent<MeshRenderer>().material.color;
+            lastColor = color;
+            color.a = .5f;
+            obstrution.gameObject.GetComponent<MeshRenderer>().material.color = color;
+            Utils.SetupMaterialWithBlendMode(obstrution.gameObject.GetComponent<MeshRenderer>().material, Utils.BlendMode.Transparent, true);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class Utils
+{
+    /*
+     * Copied from Unity Shaders: https://download.unity3d.com/download_unity/964b2488c462/builtin_shaders-2023.1.6f1.zip
+     */
+    public enum BlendMode
+    {
+        Opaque,
+        Cutout,
+        Fade,   // Old school alpha-blending mode, fresnel does not affect amount of transparency
+        Transparent // Physically plausible transparency mode, implemented as alpha pre-multiply
+    }
+    
+    /*
+     * Copied from Unity Shaders: https://download.unity3d.com/download_unity/964b2488c462/builtin_shaders-2023.1.6f1.zip
+     */
+    public static void SetupMaterialWithBlendMode(Material material, BlendMode blendMode, bool overrideRenderQueue)
+    {
+        int minRenderQueue = -1;
+        int maxRenderQueue = 5000;
+        int defaultRenderQueue = -1;
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                material.SetOverrideTag("RenderType", "");
+                material.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.One);
+                material.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetFloat("_ZWrite", 1.0f);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                minRenderQueue = -1;
+                maxRenderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest - 1;
+                defaultRenderQueue = -1;
+                break;
+            case BlendMode.Cutout:
+                material.SetOverrideTag("RenderType", "TransparentCutout");
+                material.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.One);
+                material.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetFloat("_ZWrite", 1.0f);
+                material.EnableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                minRenderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest;
+                maxRenderQueue = (int)UnityEngine.Rendering.RenderQueue.GeometryLast;
+                defaultRenderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest;
+                break;
+            case BlendMode.Fade:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetFloat("_ZWrite", 0.0f);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.EnableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                minRenderQueue = (int)UnityEngine.Rendering.RenderQueue.GeometryLast + 1;
+                maxRenderQueue = (int)UnityEngine.Rendering.RenderQueue.Overlay - 1;
+                defaultRenderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+            case BlendMode.Transparent:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.One);
+                material.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetFloat("_ZWrite", 0.0f);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                minRenderQueue = (int)UnityEngine.Rendering.RenderQueue.GeometryLast + 1;
+                maxRenderQueue = (int)UnityEngine.Rendering.RenderQueue.Overlay - 1;
+                defaultRenderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+        }
+
+        if (overrideRenderQueue || material.renderQueue < minRenderQueue || material.renderQueue > maxRenderQueue)
+        {
+            if (!overrideRenderQueue)
+                Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, "Render queue value outside of the allowed range ({0} - {1}) for selected Blend mode, resetting render queue to default", minRenderQueue, maxRenderQueue);
+            material.renderQueue = defaultRenderQueue;
+        }
+    }
+}
