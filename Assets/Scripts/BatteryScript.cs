26bd8a2970c7fc3ade9a418efd3809c45d18ee69
Scott Ho <sscott.ho@gmail.com>
2023-09-04 20:26:30 -0600

    Add local lighting, improved battery and rock sample sprites

--- Assets/Scripts/BatteryScript.cs
+++ Assets/Scripts/BatteryScript.cs
@@ -4,9 +4,40 @@ public class BatteryScript : MonoBehaviour
 {
     LevelLogicScript logic;
     bool beingDestroyed = false;
+    bool movingUp = true;
+    Vector3 down, up;
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("LevelLogic").GetComponent<LevelLogicScript>();
+        down = transform.position;
+        up = transform.position;
+        up.y += .4f;
+    }
+
+    void FixedUpdate()
+    {
+        float speed = .65f;
+        float step = speed * Time.deltaTime;
+        if (movingUp)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, up, step);
+            if (Vector3.Distance(transform.position, up) < 0.001f)
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, down, step);
+            if (Vector3.Distance(transform.position, down) < 0.001f)
+            {
+                movingUp = true;
+            }
+        }
+
+        Vector3 eulers = transform.rotation.eulerAngles;
+        eulers.z += 2.0f;
+        transform.rotation = Quaternion.Euler(eulers);
     }
 
     private void OnTriggerEnter(Collider collision)
--- Assets/Scripts/RockSampleScript.cs
+++ Assets/Scripts/RockSampleScript.cs
@@ -1,12 +1,55 @@
+using Codice.CM.Common;
 using UnityEngine;
 
 public class RockSampleScript : MonoBehaviour
 {
     LevelLogicScript logic;
     bool beingDestroyed = false;
+    bool movingUp = true;
+    Vector3 down, up;
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("LevelLogic").GetComponent<LevelLogicScript>();
+        down = transform.position;
+        up = transform.position;
+        up.y += .4f;
+        RandomizeStart();
+    }
+
+    private void RandomizeStart()
+    {
+        Vector3 pos = transform.position;
+        pos.y += Random.Range(0f, .35f);
+        transform.position = pos;
+        Vector3 eulers = transform.rotation.eulerAngles;
+        eulers.z += Random.Range(0, 45);
+        transform.rotation = Quaternion.Euler(eulers);
+    }
+
+    void FixedUpdate()
+    {
+        float speed = .65f;
+        float step = speed * Time.deltaTime;
+        if (movingUp)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, up, step);
+            if (Vector3.Distance(transform.position, up) < 0.001f)
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, down, step);
+            if (Vector3.Distance(transform.position, down) < 0.001f)
+            {
+                movingUp = true;
+            }
+        }
+
+        Vector3 eulers = transform.rotation.eulerAngles;
+        eulers.z += 2.0f;
+        transform.rotation = Quaternion.Euler(eulers);
     }
 
     private void OnTriggerEnter(Collider collision)
