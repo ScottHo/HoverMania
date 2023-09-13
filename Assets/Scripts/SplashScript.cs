55570e34976e823399d695323eda3fc5fde4cd38
Scott Ho <sscott.ho@gmail.com>
2023-09-12 21:48:55 -0600

    Add demo support

--- Assets/Scripts/SplashScript.cs
+++ Assets/Scripts/SplashScript.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Net.Http;
 
 public class SplashScript : MonoBehaviour
 {
     public RawImage image;
     public float duration;
     bool fadingIn = true;
+    bool startupFinished = false;
+
 
     private void Start()
     {
+        startupFinished = false;
         image.color = Color.black;
+        PlayerPrefs.SetInt("SelectedLevel", -1);
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            PlayerPrefs.SetInt("IsDemo", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsDemo", 0);
+        }
+
+        SyncAndWait();
     }
+
     void FixedUpdate()
     {
         if (fadingIn)
@@ -26,6 +42,42 @@ public class SplashScript : MonoBehaviour
                 FadeOut();
             }
         }
+    }
+
+    async void SyncAndWait()
+    {
+        if (PlayerPrefs.GetInt("IsDemo") == 1)
+        {
+            PlayerPrefs.SetInt("LeaderboardConnected", 0);
+            PlayerPrefs.SetInt("ShowNewUserPopup", 0);
+            PlayerPrefs.SetInt("ShowOfflinePopup", 0);
+            PlayerPrefs.SetInt("ShowDemoPopup", 1);
+            startupFinished = true;
+            return;
+        }
+        PlayerPrefs.SetInt("ShowDemoPopup", 0);
+
+        try
+        {
+            await CloudSync.GetHiScores();
+            await CloudSync.SyncCurrentUser();
+            PlayerPrefs.SetInt("LeaderboardConnected", 1);
+            int newUserFlag = 0;
+            if (DatabaseManager.Instance.database.GetUserID() == -1)
+            {
+                newUserFlag = 1;
+            }
+            PlayerPrefs.SetInt("ShowNewUserPopup", newUserFlag);
+            PlayerPrefs.SetInt("ShowOfflinePopup", 0);
+        }
+        catch (HttpRequestException e)
+        {
+            Debug.LogException(e);
+            PlayerPrefs.SetInt("LeaderboardConnected", 0);
+            PlayerPrefs.SetInt("ShowNewUserPopup", 0);
+            PlayerPrefs.SetInt("ShowOfflinePopup", 1);
+        }
+        startupFinished = true;
     }
 
     void FadeIn()
@@ -48,7 +100,7 @@ public class SplashScript : MonoBehaviour
         color.r -= .05f;
         color.g -= .05f;
         image.color = color;
-        if (color.b <= 0)
+        if (color.b <= 0 && startupFinished)
         {
             SceneManager.LoadScene("UI");
         }
